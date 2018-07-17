85ac9153955943ee87b88fb8a3503fb017e0f8cb
Jan Wielemaker <J.Wielemaker@vu.nl>
2018-07-17 13:03:39 +0200

    Initial files

---
+++ prolog/chat80.pl
@@ -0,0 +1,13 @@
+/*  File:    chat.pl
+    Author:  Jan Wielemaker
+    Created: Dec 20 2001
+    Purpose: test chat
+*/
+
+:- module(chat,
+	  [ test_chat/0,
+	    rtest_chats/1
+	  ]).
+
+:- consult('chat/chat').
+

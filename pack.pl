a9b4586383dc2a611e257faa643d5539b37face9
Jan Wielemaker <J.Wielemaker@vu.nl>
2018-07-17 16:17:16 +0200

    Unknown, but somewhat valid email

--- pack.pl
+++ pack.pl
@@ -2,8 +2,8 @@ name(chat80).
 version('1.0').
 title('Classic CHAT80 Prolog natural language application').
 keywords(['NLP', 'world']).
-author( 'Fernando C.N. Pereira', 'unknown' ).
-author( 'David H.D. Warren', 'unknown' ).
+author( 'Fernando C.N. Pereira', 'unknown@unknown' ).
+author( 'David H.D. Warren', 'unknown@unknown' ).
 packager( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
 maintainer( 'Jan Wielemaker', 'J.Wielemaker@vu.nl' ).
 home( 'https://github.com/JanWielemaker/chat80.git' ).

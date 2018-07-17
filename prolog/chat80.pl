67fbbf70171f5c2503221969c69c67dcafc4d5a5
Jan Wielemaker <J.Wielemaker@vu.nl>
2018-07-17 15:24:34 +0200

    Provide basic API for CHAT80

--- prolog/chat80.pl
+++ prolog/chat80.pl
@@ -33,10 +33,81 @@
 */
 
 :- module(chat80,
-	  [ test_chat/0,
-	    rtest_chats/1
+	  [ chat_process/2,                     % +Question, -Answer
+
+            chat_parse/2,                       % +Question, -Tree
+            chat_semantics/2,                   % +Tree, -Query
+            chat_optimize/2,			% +QueryIn, -Query
+            chat_answer/2,                      % +Query, -Answer
+
+            chat_print_tree/1,                  % +Tree
+
+            test_chat/0,
+	    rtest_chats/1                       % +Times
 	  ]).
 :- ensure_loaded(chat80/chat).
 
 /** <module> CHAT80 driver for SWI-Prolog
 */
+
+%!  test_chat
+%
+%   Run default demo suite, showing timing  and   warn  if the answer is
+%   incorrect.
+
+%!  rtest_chats(+Times)
+%
+%   Run the test  suite  Times  times   silently.  Used  for  timing and
+%   profiling.
+
+%!  chat_process(+Question, -Answer)
+%
+%   Process Question using chat80, resulting in Answer.  Question is a
+%   list of atoms expressing words.  For exampple,
+%
+%   ```
+%   ?- chat_process([what, is, the,capital, of, france, ?], A).
+%   A = [paris].
+%   ```
+%
+%   @see tokenize_atom/2.
+
+chat_process(Question, Answer) :-
+    process(Question, Answer, _Times).
+
+%!  chat_parse(+Question, -Tree)
+%
+%   Perform the parsing phase of CHAT80.
+
+chat_parse(Question, Tree) :-
+    sentence(Tree,Question,[],[],[]).
+
+%!  chat_semantics(+Tree, -Query)
+%
+%   Translate the NLP parse tree into a Prolog query.
+
+chat_semantics(Tree, Query) :-
+    i_sentence(Tree,QT),
+    clausify(QT,UE),
+    simplify(UE,Query).
+
+%!  chat_optimize(+QueryIn, -Query)
+%
+%   Optimize a query
+
+chat_optimize(QueryIn, Query) :-
+    qplan(QueryIn, Query).
+
+%!  chat_answer(+Query, -Answer)
+%
+%   Find answers for the Query.
+
+chat_answer(Query, Answer) :-
+    answer(Query, Answer).
+
+%!  chat_print_tree(+Tree)
+%
+%   Print an NLP parse tree
+
+chat_print_tree(Tree) :-
+    print_tree(Tree).
